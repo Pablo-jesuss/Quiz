a467299450883c0606d67d2ee588d0d06cda9455
Pablo-jesuss <pablo.programador26@gmail.com>
2024-03-26 18:06:18 -0300

    welcome

--- Quiz/src/App.jsx
+++ Quiz/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <div className="App">
-      <h1>Quiz App</h1>
+      <h1>Quiz de Programação</h1>
+      <Welcome />
     </div>
   );
 }
---
+++ Quiz/src/components/Welcome.jsx
@@ -0,0 +1,14 @@
+import Quiz from "../img/quiz.svg";
+
+const Welcome = () => {
+  return (
+    <div id="welcome">
+      <h2>Seja bem-vindo</h2>
+      <p>Clique no botão abaixo para começar:</p>
+      <button>Iniciar</button>
+      <img src={Quiz} alt="Início do Quiz" />
+    </div>
+  );
+};
+
+export default Welcome;

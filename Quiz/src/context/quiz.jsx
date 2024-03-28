af51b547af1fa338fc837dc5a86f59c981483e45
Pablo-jesuss <pablo.programador26@gmail.com>
2024-03-28 11:12:16 -0300

    quizState

--- Quiz/src/context/quiz.jsx
+++ Quiz/src/context/quiz.jsx
@@ -1,8 +1,28 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+import questions from "../data/questions";
+
+const STAGES = ["Start", "Playing", "End"];
+
+const initialState = {
+  gameStage: STAGES[0],
+  questions,
+};
+
+const quizReducer = (state, action) => {
+  console.log(state, action);
+
+  switch (action.type) {
+    case "CHANGE_STATE":
+      return state;
+
+    default:
+      return state;
+  }
+};
 
 export const QuizContext = createContext();
 
 export const QuizProvider = ({ children }) => {
-  const value = { name: "Quiz" };
+  const value = useReducer(quizReducer, initialState);
   return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>;
 };

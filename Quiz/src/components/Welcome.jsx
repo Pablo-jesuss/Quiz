d9e57b66eaf7b1485266552a5aa5ce25d100b369
Pablo-jesuss <pablo.programador26@gmail.com>
2024-03-28 11:16:18 -0300

    dispatch

--- Quiz/src/components/Welcome.jsx
+++ Quiz/src/components/Welcome.jsx
@@ -6,7 +6,7 @@ import Quiz from "../img/quiz.svg";
 import "./Welcome.css";
 
 const Welcome = () => {
-  const quizState = useContext(QuizContext);
+  const [quizState, dispatch] = useContext(QuizContext);
 
   console.log(quizState);
 
@@ -14,7 +14,9 @@ const Welcome = () => {
     <div id="welcome">
       <h2>Seja bem-vindo</h2>
       <p>Clique no botão abaixo para começar:</p>
-      <button>Iniciar</button>
+      <button onClick={() => dispatch({ type: "CHANCE_STATE" })}>
+        Iniciar
+      </button>
       <img src={Quiz} alt="Início do Quiz" />
     </div>
   );

fd700728b3f84e83ae30a81d9464749f8891eec2
Pablo-jesuss <pablo.programador26@gmail.com>
2024-03-28 12:22:10 -0300

    quest

--- Quiz/src/components/Quest.jsx
+++ Quiz/src/components/Quest.jsx
@@ -4,7 +4,16 @@ import { QuizContext } from "../context/quiz";
 const Quest = () => {
   const [quizState, dispatch] = useContext(QuizContext);
   console.log(quizState);
-  return <div>Question</div>;
+  return (
+    <div id="question">
+      <p>Pergunta 0 a 10</p>
+      <h2>Pergunta atual</h2>
+      <div id="options-container">
+        <p>Opções</p>
+      </div>
+      <button>Continuar</button>
+    </div>
+  );
 };
 
 export default Quest;

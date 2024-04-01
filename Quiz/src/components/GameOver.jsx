b7eda228ff2999b70584d84645689eee97049a8d
Pablo-jesuss <pablo.programador26@gmail.com>
2024-04-01 15:39:27 -0300

    gameCss

--- Quiz/src/components/GameOver.jsx
+++ Quiz/src/components/GameOver.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { QuizContext } from "../context/quiz";
 
-import WellDone from "../img/Welldone.svg";
+import WellDone from "../img/welldone.svg";
 
 import "./GameOver.css";
 

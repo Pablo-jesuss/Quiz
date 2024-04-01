a44b0bb4a34313ed0fcaf2834e21b2e502fdbadd
Pablo-jesuss <pablo.programador26@gmail.com>
2024-04-01 15:34:29 -0300

    gameOver

--- Quiz/src/components/GameOver.jsx
+++ Quiz/src/components/GameOver.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
+
+import WellDone from "../img/Welldone.svg";
+
+import "./GameOver.css";
 
 const GameOver = () => {
   return (
-    <div>
-      <h2>Game Over</h2>
+    <div id="gameover">
+      <h2>Fim de Jogo!</h2>
+      <p>Pontuação: x</p>
+      <p>Você acertou y de z perguntas.</p>
+      <img src={WellDone} alt="Fim do Quiz" />
+      <button>Reiniciar</button>
     </div>
   );
 };
